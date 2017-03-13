a7cdf2a6e62f0c89d388ad2daea8613d469c0076
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2017-03-13 12:58:37 +0530

    Put anything in VCS.

---
+++ playground.py
@@ -0,0 +1,16 @@
+from youtube_dl import YoutubeDL
+
+
+def my_progress_hook(s):
+    with open('progress.txt', 'a') as file:
+        import json
+        file.write(json.dumps(s))
+        file.flush()
+
+ytdl = YoutubeDL()
+
+ytdl.add_progress_hook(my_progress_hook)
+
+ytdl.extract_info('https://www.youtube.com/watch?v=4OctxxWGiNw')
+
+celery worker -A tasks.celery --loglevel=info
